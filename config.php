36c831f27db1f12dc6d03a502b73003c97927b78
Wolfgang Linz <webmaster@wl-lankin.de>
2018-12-01 19:08:52 +0100

    Path disclosures changed for local usage

--- config.php
+++ config.php
@@ -1,19 +1,18 @@
 <?php
-	define( 'ROOT_PATH', dirname(dirname(__FILE__)) . '/snabby/' );
-	define( 'SCRIPTS', ROOT_PATH . 'assets/scripts/' );
-	define( 'LANG', ROOT_PATH . 'assets/lang/' );
+define( 'ROOT_PATH', dirname(dirname(__FILE__)) . '/snabby/' );
+define( 'SCRIPTS', ROOT_PATH . 'assets/scripts/' );
+define( 'LANG', ROOT_PATH . 'assets/lang/' );
 
-	if (file_exists('dev.php')) {
-		$dev = true;
-	} else {
-		$dev = false;
-	}
+if (file_exists('dev.php')) {
+	$dev = true;
+} else {
+	$dev = false;
+}
 
-	if($dev) {
-		define( 'DIR_PREFIX', '//localhost/snabby' );
-		define( 'PATH_PREFIX', '/snabby' );
-	} else {
-		define( 'DIR_PREFIX', '' );
-		define( 'PATH_PREFIX', '' );
-	}
-?>
+if($dev) {
+	define( 'DIR_PREFIX', '//localhost/snabby' );
+	define( 'PATH_PREFIX', 'snabby/' );
+} else {
+	define( 'DIR_PREFIX', '' );
+	define( 'PATH_PREFIX', '' );
+}
