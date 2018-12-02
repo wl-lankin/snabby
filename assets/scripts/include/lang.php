feae866f7422a6c43efc02fee78da3dbc94f2cd9
Wolfgang Linz <webmaster@wl-lankin.de>
2018-12-02 19:19:06 +0100

    test

--- assets/scripts/include/lang.php
+++
@@ -1,9 +0,0 @@
-<?php
-	if (empty($_SERVER["HTTP_ACCEPT_LANGUAGE"])) {
-		$setlang = 'de';
-	} else {
-		$setlang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
-		$acceptlang = ['cs', 'de', 'en', 'fr', 'it', 'ja'];
-		$setlang = in_array($setlang, $acceptlang) ? $setlang : 'en';
-	}
-?>
--- assets/scripts/views/404.php
+++
@@ -1,5 +0,0 @@
-			<section class="main-content">
-				<span class="nopage">404</span>
-				<span class="nopage-text"><?php echo $lang['404'] ?></span>
-				<span class="back-link"><a href="//snabby.me"><?php echo $lang['backhome'] ?></a>
-			</section>
