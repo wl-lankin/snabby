512f8e990b7d36cb53a886019df8485e988d4a5c
Wolfgang Linz <webmaster@wl-lankin.de>
2018-12-04 23:45:35 +0100

    edited

--- config.php
+++ config.php
@@ -1,14 +1,14 @@
 <?php
-define('ROOT_PATH', dirname(dirname(__FILE__)) . '/snabby/');
-define('SCRIPTS', ROOT_PATH . 'assets/scripts/');
-define('LANG', ROOT_PATH . 'assets/lang/');
-
 $dev = (file_exists('dev.php')) ? true : false;
 
-if ($dev) {
-    define('DIR_PREFIX', '//localhost/snabby');
-    define('PATH_PREFIX', 'snabby/');
+define( 'ROOT_PATH', dirname(dirname(__FILE__)) . '/snabby/' );
+define( 'SCRIPTS', ROOT_PATH . 'assets/scripts/' );
+define( 'LANG', ROOT_PATH . 'assets/lang/' );
+
+if($dev) {
+	define( 'DIR_PREFIX', '//localhost/snabby' );
+	define( 'PATH_PREFIX', 'snabby/' );
 } else {
-    define('DIR_PREFIX', '');
-    define('PATH_PREFIX', '');
+	define( 'DIR_PREFIX', '' );
+	define( 'PATH_PREFIX', '' );
 }
